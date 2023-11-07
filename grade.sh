925eb005c9da8d5b7045898d745118ec9a760aee
Spandan2022 <2022das.spandan@gmail.com>
2023-11-07 09:49:45 -0800

    finished the script

--- grade.sh
+++ grade.sh
@@ -21,8 +21,16 @@ fi
 
 # Copy all files in student-submission to grading-area
 cp student-submission/ListExamples.java grading-area
+cp -r lib grading-area
+cp GradeServer.java grading-area
+cp Server.java grading-area
+cp TestListExamples.java grading-area
 echo 'Copied all files in student-submission into grading-area!'
 
+# Runing junit from grading-area
+cd grading-area
+javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java
+java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples
 
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point

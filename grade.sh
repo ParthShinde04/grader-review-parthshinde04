1b6f6ce22560a3d566ca1cefec41504230b0da16
Parth Shinde <48342852+ParthShinde04@users.noreply.github.com>
2023-11-28 09:13:51 -0800

    added grader

--- grade.sh
+++ grade.sh
@@ -31,3 +31,56 @@ echo 'Files moved to grading area'
 cd grading-area
 javac -cp .;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar *.java
 java -cp .;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples
+
+OUTPUT=$OUTPUT","
+
+PHASE=1
+FAILURES=0
+TESTS=0
+LENGTH=${#OUTPUT}
+TEMPSTRING=""
+for (( c=0; c<LENGTH; c++ ))
+do
+	CHAR=${OUTPUT:$c:1}
+	# echo $CHAR
+	if [[ $PHASE -eq 1 || $PHASE -eq 3 ]]
+	then
+		# Finding Semicolon for Tests Run
+		if [[ $CHAR == ":" ]]
+		then
+			PHASE=$(($PHASE+1))
+			TEMPSTRING=""
+		fi
+	elif [[ $PHASE -eq 2 || $PHASE -eq 4 ]]
+	then
+		# Getting Tests Run
+		if [[ $CHAR == "," ]]
+		then
+			if [[ $PHASE -eq 2 ]]
+			then
+				TESTS=${TEMPSTRING:1}
+			else
+				FAILURES=${TEMPSTRING:1}
+			fi
+			PHASE=$(($PHASE+1))
+		fi
+		TEMPSTRING=$TEMPSTRING$CHAR
+	fi
+done
+
+OKOUTPUT=`grep "OK" junitOutput.txt`
+if [[ ${#OKOUTPUT} != 0 ]]
+then
+	OKOUTPUT=${OKOUTPUT#*\(}
+	OKOUTPUT=${OKOUTPUT%[ ]*}
+	TESTS=$OKOUTPUT
+fi
+
+# echo "PHASE: $PHASE"
+# echo "FAILURES: $FAILURES"
+# echo "TESTS: $TESTS"
+PASSING=$(($TESTS-$FAILURES))
+PERCENT=$(((100*$PASSING)/$TESTS))
+echo "GRADE: $PERCENT% ($PASSING / $TESTS)"
+
+cd ..

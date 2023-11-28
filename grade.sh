9c7c3079745ca2e81149b1d5214c3bdd2b62b780
Parth Shinde <48342852+ParthShinde04@users.noreply.github.com>
2023-11-28 08:36:43 -0800

    made style changes

--- grade.sh
+++ grade.sh
@@ -10,6 +10,7 @@ mkdir grading-area
 git clone $1 student-submission
 echo 'Finished cloning'
 
+
 # if statement that checks if ListExamples.java exsists
 if [[ -f student-submission/ListExamples.java ]]
 then
@@ -19,21 +20,17 @@ else
     exit
 fi
 
+
 # Copy all files in student-submission to grading-area
 cp student-submission/ListExamples.java grading-area
 cp -r lib grading-area
 cp GradeServer.java grading-area
 cp Server.java grading-area
 cp TestListExamples.java grading-area
-echo 'Copied all files in student-submission into grading-area!'
+echo 'Files moved to grading area'
+
 
 # Runing junit from grading-area
 cd grading-area
 javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java
 java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples
-
-# Draw a picture/take notes on the directory structure that's set up after
-# getting to this point
-
-# Then, add here code to compile and run, and do any post-processing of the
-# tests
